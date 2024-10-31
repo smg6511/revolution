48e415ba19a004c3a5bd06ab9d14a609d2bcf774
Jim Graham <jim@pixelsandstrings.com>
2024-10-31 18:19:14 -0400

    Contexts update
    Changes to both grid list and Context editing page (general info)

--- core/src/Revolution/modContext.php
+++ core/src/Revolution/modContext.php
@@ -29,6 +29,9 @@ class modContext extends modAccessibleObject
      *  @var array RESERVED_KEYS
      */
     public const RESERVED_KEYS = ['mgr', 'web', 'root'];
+    public const CONTEXT_MANAGER = 'mgr';
+    public const CONTEXT_DEFAULT = 'web';
+    public const CONTEXT_DEFAULT_NAME = 'Website';
 
     /**
      * An array of configuration options for this context
@@ -130,7 +133,7 @@ class modContext extends modAccessibleObject
                             'context_settings'),
                         xPDO::OPT_CACHE_HANDLER => $this->xpdo->getOption('cache_context_settings_handler', null,
                             $this->xpdo->getOption(xPDO::OPT_CACHE_HANDLER, null, 'xPDO\Cache\xPDOFileCache')),
-                        xPDO::OPT_CACHE_FORMAT => (integer)$this->xpdo->getOption('cache_context_settings_format', null,
+                        xPDO::OPT_CACHE_FORMAT => (int)$this->xpdo->getOption('cache_context_settings_format', null,
                             $this->xpdo->getOption(xPDO::OPT_CACHE_FORMAT, null, xPDOCacheManager::CACHE_PHP)),
                     ]))) {
                     $context = $this->xpdo->cacheManager->generateContext($this->get('key'), $options);
@@ -202,9 +205,9 @@ class modContext extends modAccessibleObject
         $enabled = true;
         $context = !empty($context) ? $context : $this->xpdo->context->get('key');
         if (!is_object($this->xpdo->context) || $context === $this->xpdo->context->get('key')) {
-            $enabled = (boolean)$this->xpdo->getOption('access_context_enabled', null, true);
+            $enabled = (bool)$this->xpdo->getOption('access_context_enabled', null, true);
         } elseif ($this->xpdo->getContext($context)) {
-            $enabled = (boolean)$this->xpdo->contexts[$context]->getOption('access_context_enabled', true);
+            $enabled = (bool)$this->xpdo->contexts[$context]->getOption('access_context_enabled', true);
         }
         if ($enabled) {
             if (empty($this->_policies) || !isset($this->_policies[$context])) {
@@ -284,7 +287,7 @@ class modContext extends modAccessibleObject
             }
 
             if ($config['friendly_urls'] == 1) {
-                if ((integer)$id === (integer)$config['site_start']) {
+                if ((int)$id === (int)$config['site_start']) {
                     $alias = ($scheme === '' || $scheme === -1) ? $config['base_url'] : '';
                     $found = true;
                 } else {
@@ -479,5 +482,28 @@ class modContext extends modAccessibleObject
         }
 
         return $uri;
+    }
+
+    /**
+     * Returns a list of core Contexts
+     *
+     * @return array
+     */
+    public static function getCoreContexts()
+    {
+        return [
+            self::CONTEXT_MANAGER,
+            self::CONTEXT_DEFAULT
+        ];
+    }
+
+    /**
+     * @param string $key The key of the Context
+     *
+     * @return bool
+     */
+    public function isCoreContext($key)
+    {
+        return in_array($key, static::getCoreContexts(), true);
     }
 }
